91233dc73a1155f9e1cc286aab3d25bb2a466ef0
konica <prokol35@gmail.com>
2020-09-24 22:10:49 +0300

    add_8

---
+++ 4_complete_project_1/4_complete_project_1_files/c1e721094430fc80.js
@@ -0,0 +1,3 @@
+if (!window._data) window._data = {};
+        window._data['lang-static'] = {"sitemap":"https://code.s3.yandex.net/lang-static/sitemap.xml","sitemap-com":"https://code.s3.yandex.net/lang-static/sitemap-en.xml","favicon":"https://code.s3.yandex.net/lang-static/favicon.ico","id":"bunker:/wirth/lang-static#"};
+    
